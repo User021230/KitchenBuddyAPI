781ab3cfd827f4d47b071f268f70076c6c4592f4
User021230 <mgundo48@gmail.com>
2025-07-31 17:45:03 +0200

    recipe properties to string

--- KitchenBuddyAPI/Models/Recipe.cs
+++ KitchenBuddyAPI/Models/Recipe.cs
@@ -1,11 +1,11 @@
-using System.Conllections.Generic;
+using System.Collections.Generic;
 
 namespace KitchenBuddyAPI.Models;
 
 public class Recipe
 {
     public int Id { get; set; }
-    public List<string> Ingridients { get; set; }
-    public List<string> Directions { get; set; }
-    public string NutritionalBenefits { get; set; }
+    public required string Ingridients { get; set; }
+    public required string Directions { get; set; }
+    public required string NutritionalBenefits { get; set; }
 }
